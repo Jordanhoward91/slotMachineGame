93b4225176ac1782d380dc67523a564d3f33a6e4
Jordan Howard <Jordanhoward1991@gmail.com>
2023-03-01 19:28:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Slot Machine Game
+// Rules can be whatever you want
+// Symbols can be whatever you want
+// Must include a promise
+// Must include async function that awaits a promise
+// Starting balance, betting system, continue playing, Must: scoring system
+// Idea: Animate the console when you "pull the lever" 
+// // Hint: use a combination of console.log and console.clear()
+
+/*
+    * & $
+  [ $ $ $ ]
+    * * &
+*/
+
+
+// Rules
+// Characters
+// Scoring
+// Animation (shuffle) logic
